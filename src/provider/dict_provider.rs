d899f158cb2d3e2010c6af04f3f36212188eab62
ziimir <ziimir9@gmail.com>
2025-03-14 23:27:18 +0400

    up

--- src/provider/dict_provider.rs
+++ src/provider/dict_provider.rs
@@ -1,10 +1,28 @@
-use std::error::Error;
+use thiserror::Error;
 
 use reqwest::Client;
 use scraper::Html;
+use scraper::error::SelectorErrorKind;
+
+#[derive(Error, Debug)]
+pub enum DictProviderError {
+    #[error("Request error: {0}")]
+    RequestError(#[from] reqwest::Error),
+
+    #[error("Parse error: {0}")]
+    ParseError(String),
+}
+
+impl<'a> From<SelectorErrorKind<'a>> for DictProviderError {
+    fn from(err: SelectorErrorKind<'a>) -> Self {
+        DictProviderError::ParseError(format!("{:?}", err))
+    }
+}
 
 pub trait DictProvider {
+    const URL_TEMPLATE: &str;
+
     fn new(client: Client) -> Self;
 
-    async fn content(&self, query: &str) -> Result<Html, Box<dyn Error>>;
+    async fn content(&self, query: &str) -> Result<Html, DictProviderError>;
 }
--- src/provider/oxford_dict_provider.rs
+++ src/provider/oxford_dict_provider.rs
@@ -1,34 +1,42 @@
 use reqwest::Client;
 use scraper::{Html, Selector};
-use std::error::Error;
 
-use crate::provider::dict_provider::DictProvider;
-
-const OXFORD_DICTIONARY_SEARCH_URL_TEMPLATE: &str =
-    "https://www.oxfordlearnersdictionaries.com/search/english/?q={}";
-const CONTENT_SELECTOR: &str = "#entryContent";
+use crate::provider::dict_provider::{DictProvider, DictProviderError};
 
 pub struct OxfordDictProvider {
     client: Client
 }
 
+impl OxfordDictProvider {
+    const CSS_SELECTOR: &str = "#entryContent";
+}
+
 impl DictProvider for OxfordDictProvider {
+    const URL_TEMPLATE: &str = "https://www.oxfordlearnersdictionaries.com/search/english/?q={}";
+
     fn new(client: Client) -> Self {
         OxfordDictProvider { client }
     }
 
-    async fn content(&self, query: &str) -> Result<Html, Box<dyn Error>> {
-        let url = OXFORD_DICTIONARY_SEARCH_URL_TEMPLATE.replace("{}", query);
+    async fn content(&self, query: &str) -> Result<Html, DictProviderError> {
+        let url = Self::URL_TEMPLATE.replace("{}", query);
         let response = self.client.get(url).send().await?;
+
         let body = response.text().await?;
 
         let page = Html::parse_document(&body);
-        let content_selector = Selector::parse(CONTENT_SELECTOR)?;
+        let content_selector = Selector::parse(Self::CSS_SELECTOR)?;
 
         let content = page
             .select(&content_selector)
             .next()
-            .ok_or_else(|| format!("Element with selector {} not found", CONTENT_SELECTOR))?;
+            .ok_or_else(
+                || DictProviderError::ParseError(
+                    String::from(
+                        format!("Element with selector {} not found", Self::CSS_SELECTOR)
+                    )
+                )
+            )?;
 
         let fragment = Html::parse_fragment(&content.html());
         Ok(fragment)
