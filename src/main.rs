1a8d6e29c6026fe957a7e685a298d224a8bb1c49
ziimir <ziimir9@gmail.com>
2025-03-24 22:50:36 +0400

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::sync::Arc;
-use minijinja::{path_loader, Environment};
+use minijinja::Environment;
 
 mod server;
 
@@ -8,12 +8,8 @@ use server::{AppState, create_routes};
 #[tokio::main]
 async fn main() {
     let template_env = Environment::new();
-    //template_env.set_loader(path_loader("templates"));
-    //template_env.add_template("hello", "Hello {{ name }}!").unwrap();
-
     let app_state = AppState{ template_env };
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-
     axum::serve(listener, create_routes(app_state)).await.unwrap();
 }
--- src/server/routes/index/mod.rs
+++ src/server/routes/index/mod.rs
@@ -2,15 +2,15 @@ use super::AppState;
 use axum::{extract::State, routing::get, Router};
 use minijinja::context;
 
+const TEMPLATE_NAME: &str = "index";
+
 async fn handler(State(state): State<AppState>) -> String {
-    //let template = state.template_env.get_template("hello").unwrap();
-    let template = state.template_env.get_template("index").unwrap();
+    let template = state.template_env.get_template(TEMPLATE_NAME).unwrap();
     template.render(context!(name => "Dan")).unwrap().to_string()
 }
 
 pub fn create_index_router(state: &mut AppState) -> Router<AppState> {
-    //state.template_env.add_template("hello", "Hello {{ name }}!").unwrap();
-    state.template_env.add_template("index",include_str!("./index.html")).unwrap();
+    state.template_env.add_template(TEMPLATE_NAME,include_str!("./templates/index.html")).unwrap();
 
     Router::new().route("/", get(handler))
 }
